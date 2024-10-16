05cd4adf48927b55b8ccb4399c90e4b6ee478732
jayashreeagarwal <120495876+jayashreeagarwal@users.noreply.github.com>
2024-10-16 14:16:42 +0530

    pages created

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded( { extended: true } ));
 app.use(express.json());
+
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+
+const routes = require('./server/routes/drizzleRoutes.js')
+app.use('/', routes);
+
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
